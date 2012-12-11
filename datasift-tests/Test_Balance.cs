3b63a46b3d9f2794537ad2db1564e27d7d16668b
richard <richard.delorenzi@gmail.com>
2012-12-11 14:15:08 +0000

    start implementation of balance, just the framework.

---
+++ datasift-tests/Test_Balance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace datasift_tests
+{
+    [TestClass]
+    public class Test_Balance
+    {
+        [TestMethod]
+        public void Test_BalanceConstruct()
+        {
+            
+        }
+    }
+}
---
+++ datasift/Balance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace datasift
+{
+    public class Balance
+    {
+        /// <summary>
+        /// The internal data.
+        /// </summary>
+        private JSONdn m_data = null;
+
+        /// <summary>
+        /// Construct an instance from a JSONdn object.
+        /// </summary>
+        /// <param name="data">The source JSONdn object.</param>
+        public Balance(JSONdn data)
+        {
+            // Validate that the object has the required data
+            if (!data.has("balance"))
+            {
+                throw new InvalidDataException("No balance data");
+            }
+
+            // Save it for later.
+            m_data = data;
+        }
+    }
+
+
+
+}
